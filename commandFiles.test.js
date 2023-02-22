de4ce0651cb8592a0544b50fa775ee5d5ec9be58
Joel Sprouse <joel.sprouse11@gmail.com>
2023-02-22 12:54:15 +0000

    fixed test

--- commandFiles.test.js
+++ commandFiles.test.js
@@ -1,19 +1,48 @@
+/* eslint-disable no-undef */
 const fs = require('node:fs');
 const path = require('node:path');
-
-
-const commandsPath = path.join(__dirname, 'commands/CampMaster');
-// get all files in the commands folder that end with .js and do not start with an underscore
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js') && !file.startsWith('._'));
+let commandsPath, commandFiles;
 
 let i = 0;
+commandsPath = path.join(__dirname, 'commands/CampMaster');
+// get all files in the commands folder that end with .js and do not start with an underscore
+commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js') && !file.startsWith('._'));
+
 for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	console.log(`Loaded command: ${command.data.name}`.yellow);
+	console.log(file);
 	i++;
 }
 
 test('number of CampMaster commands should be 1', () => {
 	expect(i).toBe(1);
+});
+
+let j = 0;
+
+commandsPath = path.join(__dirname, 'commands/SkillCamp');
+// get all files in the commands folder that end with .js and do not start with an underscore
+commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js') && !file.startsWith('._'));
+
+for (const file of commandFiles) {
+	console.log(file);
+	j++;
+}
+
+test('number of SkillCamp commands should be 10', () => {
+	expect(j).toBe(10);
+});
+
+let k = 0;
+
+commandsPath = path.join(__dirname, 'commands/SkillCoin');
+// get all files in the commands folder that end with .js and do not start with an underscore
+commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js') && !file.startsWith('._'));
+
+for (const file of commandFiles) {
+	console.log(file);
+	k++;
+}
+
+test('number of SkillCoin commands should be 7', () => {
+	expect(k).toBe(7);
 });
